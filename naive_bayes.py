3e1aef7963ce4e4a5d37a3cd82e2c452c5d0f5e7
hyzhang <hyzhang98@gmail.com>
2017-09-17 23:52:26 +0800

    完成预测函数

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,22 +1,35 @@
 import re
 
+
+training_set_size = 1 #Laplace
+spam_set = {}
+normal_set = {}
+
+
 def main(path):
     print('start')
-    handle_email(path)
+    prepare(path_set)
+
+
+def predict(email_content):
+    p = 1.0
+    for word in email_content:
+        key_count = spam_set.get(word)
+        key_count = key_count if key_count is not None else 1 #Laplace
+        p *= key_count / training_set_size
+    return 1 if p >= 0.5 else 0
 
 
 def prepare(path_set):
     pattern = re.compile('^{data_flag:(.*?)$}')
-    spam_set = {}
-    normal_set = {}
     for path in path_set:
         raw_data = open(path)
         sample = ''
         data_flag = 0
-        handle_one_data_set(raw_data, spam_set, normal_set)
+        handle_one_data_set(raw_data, data_flag)
 
 
-def handle_one_data_set(raw_data, spam_set, normal_set, data_flag):
+def handle_one_data_set(raw_data, data_flag):
     sample = ''
     while True:
         line = raw_data.readline()
@@ -26,6 +39,7 @@ def handle_one_data_set(raw_data, spam_set, normal_set, data_flag):
         if result is None:
             sample += line
         else:
+            training_set_size += 1
             if data_flag == 0:
                 add_feature(spam_set, sample)
             else data_flag == 1:
@@ -43,7 +57,7 @@ def add_feature(pre_set, sample):
     text_set = set(text_set)
     for word in text_set:
         if pre_set.get(word) == None:
-            pre_set[word] = 1
+            pre_set[word] = 2 #Laplace
         else:
             pre_set[word] += 1
 
